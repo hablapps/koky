136c5119ca7b61039ac02fa01b30ae04281c4c30
Jesús López-González <jesus.lopez@hablapps.com>
2018-03-05 10:51:11 +0100

    Adds several MonadState lemmas

--- Core/MonadState.v
+++ Core/MonadState.v
@@ -2,8 +2,11 @@ Require Import Program.Basics.
 Require Import Monad.
 Require Export Functor.
 Require Export Monad.
+Require Import Util.FunExt.
 
 Open Scope program_scope.
+
+(* typeclass and laws *)
 
 Class MonadState (A : Type) (m : Type -> Type) `{MonadDec m} :=
 { get : m A
@@ -18,3 +21,38 @@ Class MonadStateDec (A : Type) (m : Type -> Type) `{MonadState A m} :=
 ; put_get : forall s, put s >> get = put s >> ret s
 ; put_put : forall s1 s2, put s1 >> put s2 = put s2
 }.
+
+(* theorems *)
+
+Lemma general_getget :
+  forall {m : Type -> Type} {A : Type} `{MonadStateDec A m} 
+         {X : Type} (p : A * A -> m X),
+    get >>= (fun x1 => get >>= (fun x2 => p (x1, x2))) =
+    get >>= (fun x1 => p (x1, x1)).
+Proof.
+  intros.
+  destruct H2.
+  destruct H4.
+  rewrite (functional_extensionality_with' (fun x1 => left_id _ _ (x1, x1) p)).
+  rewrite <- (assoc _ _ _ _ _ _).
+  rewrite <- get_get0.
+  rewrite -> (assoc _ _ _ _ _ _).
+  unwrap_layer.
+  rewrite -> (assoc _ _ _ _ _ _).
+  unwrap_layer.
+  now rewrite left_id.
+Qed.
+
+Lemma non_eff_get :
+  forall  {m A} `{MonadStateDec A m} {X : Type} (p : m X),
+    get >> p = p.
+Proof.
+  intros.
+  pose proof (@general_getget m A _ _ _ _ _ _).
+  destruct H2.
+  destruct H4.
+  rewrite <- (left_id _ _ tt (fun _ => p)).
+  rewrite <- get_put0.
+  rewrite (assoc _ _ _ _ _ _).
+  now rewrite (H5 _ (fun pr => put (snd pr) >> p)).
+Qed.
--- Core/Util/FunExt.v
+++ Core/Util/FunExt.v
@@ -1,6 +1,8 @@
 Require Import FunctionalExtensionality.
 Require Export FunctionalExtensionality.
 
+Ltac unwrap_layer := apply f_equal; apply functional_extensionality; intros.
+
 Theorem functional_extensionality_with :
   forall {S T} {f g : S -> T},
     (forall s, f s = g s) -> (fun s => f s) = (fun s => g s).
@@ -9,4 +11,13 @@ Proof.
   apply functional_extensionality.
   intros.
   now rewrite H.
+Qed.
+
+Theorem functional_extensionality_with' :
+  forall {S T} {f g : S -> T},
+    (forall s, g s = f s) -> (fun s => f s) = (fun s => g s).
+Proof.
+  intros.
+  symmetry in H.
+  now rewrite (functional_extensionality_with H).
 Qed.
