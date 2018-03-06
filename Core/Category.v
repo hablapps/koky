86c079fd74fd37eefbaf16e4a822b436fd27a23c
Jesús López-González <jesus.lopez@hablapps.com>
2018-03-06 13:12:15 +0100

    Adds typeclass Category

---
+++ Core/Category.v
@@ -0,0 +1,14 @@
+(* typeclass and laws *)
+
+(** A class for categories. *)
+Class Category (cat : Type -> Type -> Type) :=
+{ identity {A} : cat A A 
+; compose {A B C} : cat A B -> cat B C -> cat A C
+}.
+
+Class CategoryDec {cat} `{Category cat} :=
+{ left_id : forall A B (cab : cat A B), compose identity cab = cab
+; right_id : forall A B (cab : cat A B), compose cab identity = cab
+; assoc : forall A B C D (cab : cat A B) (cbc : cat B C) (ccd : cat C D),
+            compose cab (compose cbc ccd) = compose (compose cab cbc) ccd 
+}.
