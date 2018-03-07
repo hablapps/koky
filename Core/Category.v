d32be2cf514a5f88f4c4f6c3ab0eccaaafae8989
Jesus Lopez-Gonzalez <jesuslopezgonzalez@lavabit.com>
2018-03-07 11:09:32 +0100

    Lens category (#2)
    * Lens conforms a category

--- Core/Category.v
+++ Core/Category.v
@@ -5,8 +5,9 @@ Class Category (cat : Type -> Type -> Type) :=
 { identity {A} : cat A A 
 ; compose {A B C} : cat A B -> cat B C -> cat A C
 }.
+Notation "cab · cbc" := (compose cab cbc) (at level 40, left associativity).
 
-Class CategoryDec {cat} `{Category cat} :=
+Class CategoryDec (cat : Type -> Type -> Type) `{Category cat} :=
 { left_id : forall A B (cab : cat A B), compose identity cab = cab
 ; right_id : forall A B (cab : cat A B), compose cab identity = cab
 ; assoc : forall A B C D (cab : cat A B) (cbc : cat B C) (ccd : cat C D),
--- Core/lens.v
+++ Core/lens.v
@@ -1,4 +1,5 @@
 Require Import Program.Basics.
+Require Import Category.
 Require Import Std.prod.
 
 Hint Resolve prod_proj.
@@ -8,31 +9,73 @@ Open Scope program_scope.
 (** Polymorphic lens: views the current part [A] from the whole [S] and updates 
     the part [A] with a new version of it [B], which is reflected in the whole 
     [T]. *)
-Record pLens (S T A B : Type) := mkLens
+Record pLens (S T A B : Type) := mkPLens
 { view : S -> A
 ; update : S -> B -> T
 }.
-Arguments mkLens [S T A B].
+Arguments mkPLens [S T A B].
 Arguments view [S T A B].
 Arguments update [S T A B].
 
 (** Monomorphic lens *)
 Definition lens S A : Type := pLens S S A A.
 
-Record lensDec {S A} (ln : lens S A) :=
+Record lensDec {S A} (ln : lens S A) := mkLensDec
 { view_update : forall s, update ln s (view ln s) = s
 ; update_view : forall s a, view ln (update ln s a) = a
 ; update_update : forall s a1 a2, update ln (update ln s a1) a2 = update ln s a2
 }.
 
-Definition composeLens {S A B} (ln1 : lens S A) (ln2 : lens A B) : lens S B :=
-  mkLens (view ln2 ∘ view ln1)
-         (fun s => update ln1 s ∘ update ln2 (view ln1 s)).
-Notation "ln1 ▷ ln2" := (composeLens ln1 ln2) (at level 40, left associativity).
+Instance Category_lens : Category lens :=
+{ identity _ := mkPLens id (fun _ => id) 
+; compose _ _ _ ln1 ln2 := mkPLens 
+    (view ln2 ∘ view ln1)
+    (fun s => update ln1 s ∘ update ln2 (view ln1 s))
+}.
+
+Instance CategoryDec_lens : CategoryDec lens.
+Proof.
+  split; simpl; intros; destruct cab; auto.
+Qed.
+
+(** Very well behaved lens: lens and laws packed together. *)
+Record vwbLens S A := mkVwbLens
+{ ln : lens S A
+; lnDec : lensDec ln
+}.
+Arguments mkVwbLens [S A].
+Arguments ln [S A].
+Arguments lnDec [S A].
+
+Lemma lensDec_identity : forall A, lensDec (@identity lens _ A).
+Proof. split; auto. Qed.
+
+Lemma lensDec_compose :
+  forall {A B C} (ln1 : lens A B) (ln2 : lens B C), 
+    lensDec ln1 -> lensDec ln2 -> lensDec (compose ln1 ln2).
+Proof.
+  intros.
+  destruct H.
+  destruct H0.
+  split; simpl; unfold Basics.compose; intros.
+
+  - (* view_update *)
+    rewrite view_update1.
+    now rewrite view_update0.
+
+  - (* update_view *)
+    rewrite update_view0.
+    now rewrite update_view1.
+
+  - (* update_update *)
+    rewrite update_update0.
+    rewrite update_view0.
+    now rewrite update_update1.
+Qed.
 
 (** Provides access to the first component of a product. *)
 Definition first {A B} : lens (A * B) A :=
-  mkLens fst (fun s a' => (a', snd s)).
+  mkPLens fst (fun s a' => (a', snd s)).
 
 Definition firstDec {A B} : lensDec (@first A B).
 Proof.
@@ -41,7 +84,7 @@ Qed.
 
 (** Provides access to the second component of a product. *)
 Definition second {A B} : lens (A * B) B :=
-  mkLens snd (fun s b' => (fst s, b')).
+  mkPLens snd (fun s b' => (fst s, b')).
 
 Definition secondDec {A B} : lensDec (@second A B).
 Proof.
